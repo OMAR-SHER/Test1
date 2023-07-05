f512e50c16f0dbed195b845642639596f2f7a970
omar <136499986+OMAR-SHER@users.noreply.github.com>
2023-07-06 00:42:06 +0500

    Add files via upload

--- Main1.cpp
+++ Main1.cpp
@@ -1,53 +1,89 @@
-#include"Linklist.h"
+#include<iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+void SortedInsert(struct Node** H, int x)
+{
+    struct Node* t, * q = NULL, * p = *H;
+
+    t = new Node;
+    t->data = x;
+    t->next = NULL;
+
+    if (*H == NULL)
+        *H = t;
+    else
+    {
+        while (p && p->data < x)
+        {
+            q = p;
+            p = p->next;
+        }
+        if (p == *H)
+        {
+            t->next = *H;
+            *H = t;
+        }
+        else
+        {
+            t->next = q->next;
+            q->next = t;
+        }
+    }
+
+}
+void  Search(struct Node* p, int key)
+{
+    bool valid = true;
+    while (p != NULL)
+    {
+        if (key == p->data)
+        {
+            valid = false;
+            cout << "data found : " << p->data << endl;
+            //return p;
+        }
+        p = p->next;
+    }
+    //return NULL;
+    if (valid == true)
+    {
+        cout << "data not found\n";
+    }
+
+}
+int hash1(int key)
+{
+    return key % 10;
+}
+void Insert(struct Node* H[], int key)
+{
+    int index = hash1(key);
+    SortedInsert(&H[index], key);
+}
 int main()
 {
-	int op;
-	string word;
-	Alphabets dict;
-	//Alphabets A;
-	do {
-		cout << " 1. Search meanings \n 2. Delete word \n 3. Add word \n 4. Display all words \n 5. Exit " << endl;
-		cin >> op;
-		if (op == 1)
-		{
-			cout << "Enter the word" << endl;
-			cin >> word;
-			fstream file = dict.open_file_R(word);
-			//file open wala functiononononomioimmoniin
-			dict = dict.readFile(file);//do not comment!!
-			//file read fn returns obj
-			//dict = obj
-			dict.search_wrd(word);//do not comment!!
-		}
-		else if (op == 2)
-		{
-			cout << "Enter the word" << endl;
-			cin >> word;
-			dict.delete_wrd(word);
+    struct Node* HT[10];
+    struct Node* temp;
+    int i;
 
-		}
-		else if (op == 3)
-		{
-			cout << "Enter the word" << endl;
-			cin >> word;
-			dict.writefile(word);
-			//dict.add_wrd(word);
-			//dict.writefile();
-		}
-		else if (op == 4)
-		{
-			dict.print_wrd_list();
-		}
-		else if (op == 5)
-		{
-			exit;
-		}
-		else
-		{
-			cout << "enter invalid choice";
-		}
-		system("pause");
-		system("cls");
-	} while (op != 5);
-	return 0;
+    for (i = 0; i < 10; i++)
+        HT[i] = NULL;
+
+    Insert(HT, 12);
+    Insert(HT, 22);
+    Insert(HT, 22);
+    Insert(HT, 42);
+    Insert(HT, 17);
+
+    //temp =
+     Search(HT[hash1(22)], 22);
+     Search(HT[hash1(22)], 17);
+     Search(HT[hash1(7)], 17);
+   // cout << temp->data;
+
+    return 0;
 }
